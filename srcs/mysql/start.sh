402e1c5fdecf3600fd2e55d51c1a5b16cf152c27
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-08-22 13:32:27 +0300

    wordpress works, now will separate phpmyadmin from wordpress container

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -6,20 +6,20 @@
 #    By: farhod <farhod@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/13 18:12:35 by btammara          #+#    #+#              #
-#    Updated: 2021/08/22 13:15:23 by farhod           ###   ########.fr        #
+#    Updated: 2021/08/22 10:10:37 by farhod           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 # service nginx start
-# service mysql start
-service php7.3-fpm start
+service mysql start
+# service php7.3-fpm start
 # /usr/sbin/php-fpm7.3 -y /etc/php/7.3/fpm/php-fpm.conf
 
-# echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
-# echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' identified by 'admin' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
 # echo "UPDATE mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
 # mysql -u root -p wordpress --skip-password < wordpress.sql
 
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'admin' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -77,7 +77,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
